669362351a70ec7eba8bbc7506f589e5d916b762
sinalilutz-crypto <sina.li.lutz@gmail.com>
2025-12-16 10:24:28 +0100

    render.sh

--- render.sh
+++ render.sh
@@ -22,6 +22,7 @@ ffmpeg \
 -loop 1 -i "3.jpg" \
 -loop 1 -i "4.JPG" \
 -filter_complex "
+-filter_complex "
 [1:v]scale=iw*0.7:ih*0.7,format=rgba,zoompan=z='1.0+0.0007*n':d=$((${INTRO_D}*${FPS})):s=${W}x${H}[logo];
 
 [0:v][logo]overlay=(W-w)/2:(H-h)/2:enable='between(t,0,${INTRO_D})'[s1];
@@ -30,6 +31,9 @@ ffmpeg \
 x=(w-text_w)/2:y=(h-text_h)/2:
 fontsize=54:fontcolor=white:line_spacing=10:
 enable='between(t,${INTRO_D},$((${INTRO_D}+${TEXT_D})) )'[s2];
+
+[2:v]scale=${W}:${H}:force_original_aspect_ratio=increase,crop=${W}:${H}[grp];
+[s2][grp]overlay=0:0:enable='between(t,$((${INTRO_D}+${TEXT_D})),$((${INTRO_D}+${TEXT_D}+${GRUPPE_D})) )'[s3];
 
 [s3]drawtext=fontfile=${FONT}:text='Danke für das entgegengebrachte Vertrauen':
 x=(w-text_w)/2:y=(h-text_h)/2:
@@ -45,12 +49,15 @@ enable='between(t,$((${INTRO_D}+${TEXT_D}+${GRUPPE_D})),$((${INTRO_D}+${TEXT_D}+
 [x1][t3]xfade=transition=fade:duration=1:offset=$((${TEAM_START}+4))[x2];
 [x2][t4]xfade=transition=fade:duration=1:offset=$((${TEAM_START}+8))[x3];
 
-[x3]eq=contrast=1.02:brightness=0.01:saturation=1.05,
+[s4][x3]overlay=0:0:enable='between(t,${TEAM_START},${TOTAL})'[mix];
+
+[mix]eq=contrast=1.02:brightness=0.01:saturation=1.05,
 colorbalance=rs=0.015:gs=0.005:bs=-0.010,
 fade=t=in:st=0:d=0.8,
 fade=t=out:st=$((${TOTAL}-1)):d=1
 [v]
 " \
+
 -map "[v]" \
 -t ${TOTAL} \
 -c:v libx264 -pix_fmt yuv420p -r ${FPS} \
